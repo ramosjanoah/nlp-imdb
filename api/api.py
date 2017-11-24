7cfccc404fc3429c5ef5b595e6623a447a1c22f5
alsoncahyadi <alson.cahyadi@gmail.com>
2017-11-24 11:45:38 +0700

    Added api

---
+++ api/api.py
@@ -0,0 +1,22 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from flask import Flask, json, request
+from s6_scrape_score import score_imdb_url
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+	return "Welcome!"
+
+@app.route("/svm", methods=['POST'])
+def svm():
+	url = request.form.get('url', 'NONE')
+	scrape_limit = request.form.get('scrape_limit', 'NONE')
+	clf = 'svm'
+	score = score_imdb_url(url, scrape_limit=scrape_limit, clf=clf)
+	print("DONEEEEEEEEEEEEEEEEEEEEEEEEe")
+	return str(score)
+
+if __name__ == "__main__":
+	app.run()
